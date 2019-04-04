2a2923c29ce272000ffddce3f22f8ae07605a9fa
Mats Grimsgaard <mg@vofo.no>
2019-04-04 13:27:18 +0000

    Remove layout and unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 MinikasPayable::Engine.routes.draw do
   resources :batches, only: %w[index show create update destroy]
-  resources :transfers, only: %w[create destroy]
+  resources :transfers, only: %w[destroy]
 end
